b4ce97b6a700a12f31ed11223fab6965384b27f3
Oliver Stenkilde <hello@stenkil.de>
2023-02-06 17:19:28 +0100

    chore: added the last mods for the base integration

--- src/data/sample.ts
+++ src/data/sample.ts
@@ -139,6 +139,135 @@ export const mods: Mod[] = [
 		},
 		groupId: null,
 	},
+	{
+		type: 'pistol',
+		mode: 'relative',
+		name: 'Pistol Gambit',
+		stats: [],
+		multiplier: 1.2,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: 'gambit',
+	},
+	{
+		type: 'pistol',
+		mode: 'relative',
+		name: 'Pistol Gambit',
+		stats: [],
+		multiplier: 1.87,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: 'gambit',
+	},
+	{
+		type: 'pistol',
+		mode: 'relative',
+		name: 'Creeping Bullseye',
+		stats: [],
+		multiplier: 2,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: null,
+	},
+	{
+		type: 'pistol',
+		mode: 'relative',
+		name: 'Hydraulic Crosshairs',
+		stats: [],
+		state: {
+			selected: false,
+			condition: [
+				{
+					state: false,
+					description: 'On headshot when aiming gain Critical Hit',
+					multiplier: 1.35,
+				},
+			],
+		},
+		groupId: 'crosshairs',
+	},
+	{
+		type: 'pistol',
+		mode: 'relative',
+		name: 'Galvanized Crosshair',
+		stats: [],
+		state: {
+			selected: false,
+			condition: [
+				{
+					state: false,
+					description: 'On headshot when aiming gain Critical Hit',
+					multiplier: 1.2,
+				},
+				{
+					state: false,
+					description: 'On headshot kills when aiming gain Critical Hit',
+					multiplier: 0.4,
+				},
+			],
+		},
+		groupId: 'crosshairs',
+	},
+	{
+		type: 'melee',
+		mode: 'relative',
+		name: 'True Steel',
+		stats: [],
+		multiplier: 1.2,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: 'steel',
+	},
+	{
+		type: 'melee',
+		mode: 'relative',
+		name: 'Maiming Strike',
+		stats: [],
+		multiplier: 1.5,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: null,
+	},
+	{
+		type: 'melee',
+		mode: 'relative',
+		name: 'Sacrificial Steel',
+		stats: [],
+		multiplier: 2.2,
+		state: {
+			selected: false,
+			condition: [],
+		},
+		groupId: 'steel',
+	},
+	{
+		type: 'melee',
+		mode: 'relative',
+		name: 'Mark of the Beast',
+		stats: [],
+
+		state: {
+			selected: false,
+			condition: [
+				{
+					multiplier: 1.2,
+					state: false,
+					description: 'On 6 melee kills with 6 seconds gain Critical Hit',
+				},
+			],
+		},
+		groupId: null,
+	},
 ];
 
 export const buffs: Mod[] = [
